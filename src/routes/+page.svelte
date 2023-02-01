a8ca46415fbc03fed6f7d2d0622ae6e5c53ce820
Alice <123740496+etehad@users.noreply.github.com>
2023-02-01 08:14:00 +0000

    More layout improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,23 @@
-<div class="fixed flex h-full flex-col justify-between pt-6 pb-8">
-  <div>
-    <h1 class="text-center text-xl font-semibold">اتحادیه انقلاب مهر</h1>
-    <p class="mx-4 mt-6 leading-relaxed">
-      در مهر ماه ۱۴۰۱ برای همگان روشن شد در افکار ایرانیان انقلابی جدید رخ داده است. ما می‌خواهیم
-      اتحادیه‌‌ای سازمان‌یافته با رهبری شورایی از متخصصین و فعالینِ انقلاب برای پیروزی تشکیل شود. ما
-      هر حال رشد هستیم. در ابتدا هدف فقط رشد اتحادیه و تشکیل شورای هماهنگی است. همانند کشوری که در
-      حال بزرگ‌ کردن و تقویت ارتش‌ خودش است ولی هنوز حمله نکرده.
-    </p>
-  </div>
+<div class="fixed flex h-full max-w-screen-md flex-col justify-between">
+  <h1 class="mt-7 text-center text-xl font-semibold">اتحادیه انقلاب مهر</h1>
 
-  <div class="mx-4 mb-4 flex flex-col items-center">
+  <div class="mx-4 flex flex-col items-center">
     <div class="mb-2 text-2xl">بیش از ۱۰ نفر</div>
     <div>عضو فعال در اتحادیه تا کنون</div>
   </div>
 
-  <div class="flex w-full flex-col items-center gap-4 px-4">
-    <p>شما هم به اتحادیه بپیوندید تا با هم هدفمند، سازمان&nbsp;یافته، و متحد عمل کنیم.</p>
-    <a href="/intro" class="rounded-md bg-slate-200 py-2 px-4 dark:bg-slate-800"
+  <div class="flex w-full flex-col items-center px-4">
+    <p class="w-full leading-relaxed">
+      در مهر ماه ۱۴۰۱ برای همگان روشن شد در افکار ایرانیان انقلابی جدید رخ داده است. ما می‌خواهیم
+      اتحادیه‌‌ای سازمان‌یافته با رهبری شورایی از متخصصین و فعالینِ انقلاب برای پیروزی تشکیل شود. ما
+      هر شب رأس ساعت ۲۱ عبارت «اتحادیه انقلاب مهر» را در گوگل جست‌وجو می‌کنیم تا نشان دهیم وجود
+      داریم و در حال رشد هستیم. در ابتدا هدف فقط رشد اتحادیه و تشکیل شورای هماهنگی است. همانند کشوری
+      که در حال بزرگ‌ کردن و تقویت ارتش‌ خودش است ولی هنوز حمله نکرده.
+    </p>
+    <p class="mt-2 w-full">
+      شما هم به اتحادیه بپیوندید تا با هم هدفمند، سازمان&nbsp;یافته، و متحد عمل کنیم.
+    </p>
+    <a href="/intro" class="mt-6 mb-8 rounded-md bg-slate-200 py-2 px-4 dark:bg-slate-800"
       >از اینجا شروع کنید</a>
   </div>
 </div>
